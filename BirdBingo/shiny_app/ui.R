5c698e6d9fa3d7b69cc892745337aa7c6bec6e73
Tamora James <tamora.james@tiscali.co.uk>
2016-03-15 18:04:35 +0000

    Start to put together the final app

--- BirdBingo/shiny_app/ui.R
+++ BirdBingo/shiny_app/ui.R
@@ -15,14 +15,14 @@ shef_data <- shef_data[shef_data$pTaxonVersionKey %in% birdTVKs,]
 spp <- table(shef_data$pTaxonName)
 spp <- spp[order(spp, decreasing = T)]
 
-spp.names <- names(spp)[1:25]
+spp.names <- names(spp)[1:50]
 
 sppdat <- MatchTaxatoEOLID(spp.names, exact = T)
 sppdat <- sppdat[!is.na(sppdat$eolPageNumbers),]
 
 spp <- spp[names(spp) %in% sppdat$ListOfTaxa]
 
-spp.list <- as.numeric(sample(sppdat$eolPageNumbers, size = 10, prob = spp, replace=F))
+spp.list <- as.numeric(sample(sppdat$eolPageNumbers, size = 25, prob = spp, replace=F))
 
 chosen.names <- sppdat$ListOfTaxa[match(spp.list, sppdat$eolPageNumbers)]
 
@@ -68,70 +68,79 @@ for (i in 1:length(myEOL)) {
 
 img.height <- "200px"
 img.width <- "150px"
+grid.size <- 4
+col.width <- 3
 
 ui <- fluidPage(
     # Application title
     titlePanel("Bird Bingo!"),
     hr(),
-    fluidRow(
-        column(2, h5(chosen.names[1])),
-        column(2, h5(chosen.names[2])),
-        column(2, h5(chosen.names[3])),
-        column(2, h5(chosen.names[4])),
-        column(2, h5(chosen.names[5])),
-        column(2, h5(chosen.names[6]))
-        ),
-    fluidRow(
-        column(2,
-               img(src=url.list[[1]][1], width=img.width),
-               img(src=url.list[[1]][2], width=img.width),
-               img(src=url.list[[1]][3], width=img.width),
-               img(src=url.list[[1]][4], width=img.width)
-               ),
-        column(2,
-               img(src=url.list[[2]][1], width=img.width),
-               img(src=url.list[[2]][2], width=img.width),
-               img(src=url.list[[2]][3], width=img.width),
-               img(src=url.list[[2]][4], width=img.width)
-        ),
-        column(2,
-               img(src=url.list[[3]][1], width=img.width),
-               img(src=url.list[[3]][2], width=img.width),
-               img(src=url.list[[3]][3], width=img.width),
-               img(src=url.list[[3]][4], width=img.width)
-        ),
-        column(2,
-               img(src=url.list[[4]][1], width=img.width),
-               img(src=url.list[[4]][2], width=img.width),
-               img(src=url.list[[4]][3], width=img.width),
-               img(src=url.list[[4]][4], width=img.width)
-        ),
-        column(2,
-               img(src=url.list[[5]][1], width=img.width),
-               img(src=url.list[[5]][2], width=img.width),
-               img(src=url.list[[5]][3], width=img.width),
-               img(src=url.list[[5]][4], width=img.width)
-        ),
-        column(2,
-               img(src=url.list[[6]][1], width=img.width),
-               img(src=url.list[[6]][2], width=img.width),
-               img(src=url.list[[6]][3], width=img.width),
-               img(src=url.list[[6]][4], width=img.width)
-        )
-    ),
-    hr(),
-    fluidRow(
-        column(2, checkboxInput("Seen1", label = "Seen it?", value = FALSE)),
-        column(2, checkboxInput("Seen2", label = "Seen it?", value = FALSE)),
-        column(2, checkboxInput("Seen3", label = "Seen it?", value = FALSE)),
-        column(2, checkboxInput("Seen4", label = "Seen it?", value = FALSE)),
-        column(2, checkboxInput("Seen5", label = "Seen it?", value = FALSE)),
-        column(2, checkboxInput("Seen6", label = "Seen it?", value = FALSE))
-        )
+    lapply(1:grid.size, function(i) {
+      fluidRow(
+        lapply(1:grid.size, function(j) {
+          column(col.width,
+                 h5(chosen.names[(i-1)*grid.size+j]),
+                 imageOutput(paste0("image", i, ".", j),
+                             width=img.width, height=img.height,
+                             click = paste0("image_click", i, ".", j)),
+                 textOutput(paste0("image_info", i, ".", j)))
+        })
+      )
+    }),
+    textOutput("grid_check_info")
 )
 
+checkGrid <- function(input) {
+  grid <- array(dim = rep(grid.size,2))
+  for (i in 1:grid.size) {
+    for (j in 1:grid.size) {
+      grid[i,j] <- !is.null(input[[paste0('image_click', i, ".", j)]]$x)
+    }
+  }
+  res <- any(apply(grid, MARGIN=1, all)) |
+    any(apply(grid, MARGIN=2, all)) |
+    all(diag(grid)) | all(diag(grid[,grid.size:1]))
+
+  return(res)
+}
+
 server <- function(input, output, session) {
-    
+
+  output$grid_check_info <- renderPrint({
+    if (checkGrid(input)) {
+      cat("BINGO!\n")
+    }
+  })
+
+  # Image output:
+  lapply(1:grid.size, function(i) {
+    lapply(1:grid.size, function(j) {
+      imgId <- paste0('image', i, ".", j)
+      output[[imgId]] <- renderImage({
+        index <- (i-1)*grid.size+j
+        imageInfo <- url.list[[index]]
+
+        # A temp file to save the output
+        outfile <- tempfile(fileext='.jpg')
+        # TODO catch problem downloading image
+        download.file(imageInfo$mediaUrl, outfile)
+
+        list(
+          src = outfile,
+          width = img.width,
+          contentType = "image/jpeg",
+          alt = chosen.names[index]
+        )
+      }, deleteFile = FALSE)
+
+      # interaction click in image
+      observeEvent(input[[paste0('image_click', i, ".", j)]], {
+        output[[paste0('image_info', i, ".", j)]] <- renderPrint({
+          cat("Seen it!\n")
+        })
+      })
+    })
+  })
 }
 
 shinyApp(ui, server)
